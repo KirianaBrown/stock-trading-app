e691049f4cad604680ff1a40056bd345c2758a22
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-07-08 12:47:46 +1200

    add buy/sell option to dashboard

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,18 +4,18 @@ function openCity(evt, action) {
     let i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("tab-links");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace("tab-active", "");
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(action).style.display = "block";
-    evt.currentTarget.className += " active";
+    evt.currentTarget.className += " tab-active";
 }
