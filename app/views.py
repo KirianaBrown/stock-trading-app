eb5b9fded8c6a9b9db106bc45d08f4b782b23aab
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-07-12 15:39:24 +1200

    remove random int fn

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from .models import db, User
 # Helper Functions
-from .helpers import check_registration_valid, getListGainers, getListMostActive, getListLosers, getLogo, formatDollar, formatPercentage, randomInt
+from .helpers import check_registration_valid, getListGainers, getListMostActive, getListLosers, getLogo, formatDollar, formatPercentage, randomInt, getCryto
 
 
 from dotenv import load_dotenv
@@ -114,10 +114,13 @@ def dashboard():
     spotlight = session['listMostActive'][0]
 
     # 3. get bitcoin
+    bitcoin = getCryto('btcusd')
 
-    # 4. get gold
+    # 4. get cardona
+    cardano = getCryto('adausd')
 
     # 5. get etherirum
+    eth = getCryto('ethusd')
 
     # 6. get portfolio
 
