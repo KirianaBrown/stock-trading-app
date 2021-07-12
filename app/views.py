9d5075d654a04e0e080b5240dd628bd17214395a
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-07-12 16:42:59 +1200

    import getCompanyDetails

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from .models import db, User
 # Helper Functions
-from .helpers import check_registration_valid, getListGainers, getListMostActive, getListLosers, getLogo, formatDollar, formatPercentage, getCryto
+from .helpers import check_registration_valid, getListGainers, getListMostActive, getListLosers, getLogo, formatDollar, formatPercentage, getCryto,getCompanyDetails
 
 
 from dotenv import load_dotenv
