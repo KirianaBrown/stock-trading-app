7685c9ef922318fe028dca409b67c6a64c20d840
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-07-15 11:34:10 +1200

    set up logic to handle topping up and withdrawing from wallet

--- app/account.py
+++ app/account.py
@@ -8,25 +8,31 @@ def wallet(action):
   if request.method == 'POST':
     # 1. get amount from form
     req = request.form
-    amount = req.get('amount')
+    amount = float(req.get('amount'))
 
     if not amount:
       return 'No amount provided'
 
+    # 2. get wallet for current user
+    wallet = 1000
+
     if action == 'topup':
-      return f'topup selected with an amount of {amount}'
+      # add value to the wallet
+      balance = wallet + amount
+      # render the account page (GET) which will add the transaction and flash('success message')
+      return f'topup selected with an amount of {amount} to leave a new wallet balance of {balance}'
     
     if action == 'withdrawal':
-      return f'withdrawal selected with an amount of {amount}'
+      # validate withdrawal value does not exceed wallet
+      if amount < wallet:
+        # update balance to show withdrawal
+        balance = wallet - amount
+        # render account page(GET) which will add transaction and flash success message
+        return f'withdrawal selected with an amount of {amount} from wallet {wallet} leaving a balance of {balance}'
+      else:
+        return f'Unable to withdrawal value of {amount} due to it exceeding current wallet balance of {wallet}'
 
-    # 2. get wallet for current user
-
-    # 3. Add value to the wallet
-
-    # 4. Log transaction date (render account page)
-    # return render_template('/account.html')
-
-  return 'top up the wallet'
+  return redirect('/account')
 
 
 @app.route('/password', methods=['GET', 'POST'])
