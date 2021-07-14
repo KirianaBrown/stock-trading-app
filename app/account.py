bf223044874775e4a8fbe06ac54f5c709110c1b9
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-07-15 10:46:05 +1200

    set up password change route

--- app/account.py
+++ app/account.py
@@ -3,7 +3,11 @@ from .models import db
 from flask.helpers import url_for
 from flask import Flask, redirect, render_template, request, session, flash
 
-
 @app.route('/wallet', methods=['GET', 'POST'])
 def wallet():
   return 'top up the wallet'
+
+
+@app.route('/password', methods=['GET', 'POST'])
+def password():
+  return 'change password'
