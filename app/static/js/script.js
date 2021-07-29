e67d90def4170a7dec61a2f3ecb41c2cc0e38b38
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-07-30 11:27:32 +1200

    add accordian styles to account leaving only the wallet information

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -42,3 +42,19 @@ onReady(function() {
     setVisible(".dashboard-content-content", true);
     setVisible(".loader", false);
 });
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("accordian-selected");
+
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
