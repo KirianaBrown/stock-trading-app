dc42a11675a5c5af343843062f41a5464607d637
KirianaBrown <kjb0369@arastudent.ac.nz>
2021-08-03 15:46:07 +1200

    remove testing parameter

--- app/static/gen/all.js
+++ app/static/gen/all.js
@@ -3,19 +3,16 @@ console.log("hello from the console");
 function switchTabs(evt, action) {
     let i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-
-    // Get all elements with class="tablinks" and remove the class "active"
+    c;
     tablinks = document.getElementsByClassName("tab-links");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace("tab-active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(action).style.display = "block";
     evt.currentTarget.className += " tab-active";
 }
